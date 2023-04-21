3af909dd7b670cb6a8c29e0a5ef91efb5df09267
Feddyz <69622868+Feddyz@users.noreply.github.com>
2023-04-21 16:58:17 +0800

    Update Coffee.java
    删除了所有static修饰符

--- GourmetCoffeeSystem/src/base/Coffee.java
+++ GourmetCoffeeSystem/src/base/Coffee.java
@@ -1,72 +1,61 @@
 package base;
 
 public class Coffee extends CoffeeAccessory {
-    static String origin = "";
-    static String roast = "";
-    static String flavor = "";
-    static String aroma = "";
-    static String acidity = "";
-    static String body = "";
-    static String smallCharge ="";
+    String countryoforigin = "";
+    String roast = "";
+    String flavor = "";
+    String aroma = "";
+    String acidity = "";
+    String body = "";
+    String smallCharge ="";
 
-    public Coffee(String inicode, String inidescription, Double iniprice,String iniorigin,String iniroast,String iniflavor,String iniaroma,String iniacidity,String inibody,String inismallCharge) {
-        super(inicode, inidescription, iniprice);
-        this.origin=iniorigin;
-        this.roast=iniroast;
-        this.flavor=iniflavor;
-        this.aroma=iniaroma;
-        this.acidity=iniacidity;
-        this.body=inibody;
-        this.smallCharge=inismallCharge;
+//get
+    public String getOrigin(){
+        return countryoforigin;
     }
-
-    //get
-    public static String getOrigin(){
-        return origin;
-    }
-    public static String getRoast(){
+    public String getRoast(){
         return roast;
     }
-    public static String getFlavor(){
+    public String getFlavor(){
         return flavor;
     }
-    public static String getSmallCharge(){
+    public String getSmallCharge(){
         return smallCharge;
     }
-    public static String getAroma(){
+    public String getAroma(){
         return aroma;
     }
-    public static String getAcidity(){
+    public String getAcidity(){
         return acidity;
     }
-    public static String getBody(){
+    public String getBody(){
         return body;
     }
     //set
-    public static void setOrigin(String newOrigin){
-        origin = newOrigin;
+    public void setOrigin(String newOrigin){
+        countryoforigin = newOrigin;
     }
-    public static void setRoast(String newRoast){
+    public void setRoast(String newRoast){
         roast = newRoast;
     }
-    public static void setFlavor(String newFlavour){
+    public void setFlavor(String newFlavour){
         flavor= newFlavour;
     }
-    public static void setSmallCharge(String newSmallCharge){
+    public void setSmallCharge(String newSmallCharge){
         smallCharge = newSmallCharge;
     }
-    public static void setAroma(String newAroma){
+    public void setAroma(String newAroma){
         aroma = newAroma;
     }
-    public static void setAcidity(String newAcidity){
+    public void setAcidity(String newAcidity){
         acidity = newAcidity;
     }
-    public static void setBody(String newBody){
+    public void setBody(String newBody){
         body = newBody;
+        ;
     }
 
-    @Override
-    public String toString() {
-        return super.toString()+"_"+getOrigin()+"_"+getRoast()+"_"+getFlavor()+"_"+getSmallCharge()+"_"+getAroma()+"_"+getAcidity()+"_"+getBody();
-    }
+
+
+
 }
