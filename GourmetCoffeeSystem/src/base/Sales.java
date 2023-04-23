7948c3c7003781a87edc8c8037c11c4475b4b026
ThekinkA <110724495+ThekinkA@users.noreply.github.com>
2023-04-23 16:36:22 +0800

    Update Sales.java

--- GourmetCoffeeSystem/src/base/Sales.java
+++ GourmetCoffeeSystem/src/base/Sales.java
@@ -1,13 +1,27 @@
+package base;
+
+import base.Order;
+
 import java.util.ArrayList;
-public class Saled implements Iterable<Order>{
-  private ArrayList<Order>orders;
-  public Sales(){
-    this.orders=new ArrayList<Order>();
-  }
-  public xiod addOrder(Order order){
-    this.order.add(order);
-  }
-  public int getNumberOfOrderd(){
-    return this.order.size();
-  }
+import java.util.Iterator;
+
+public class Sales implements Iterable<Order> {
+    private ArrayList<Order> orders;
+
+    public Sales() {
+        this.orders = new ArrayList<Order>();
+    }
+
+    public void addOrder(Order order) {
+        this.orders.add(order);
+    }
+
+    public int getNumberOfOrders() {
+        return this.orders.size();
+    }
+
+    @Override
+    public Iterator<Order> iterator() {
+        return this.orders.iterator();
+    }
 }
