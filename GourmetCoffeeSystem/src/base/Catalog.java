c4a7915bc2dcc3ec654b7eda7675e0dd10caa7da
ThekinkA <110724495+ThekinkA@users.noreply.github.com>
2023-04-23 16:33:41 +0800

    Update Catalog.java

--- GourmetCoffeeSystem/src/base/Catalog.java
+++ GourmetCoffeeSystem/src/base/Catalog.java
@@ -1,23 +1,38 @@
 package base;
 
-public Catalog() {
-  this.products=new ArrayList<Product>();
-}
-public viod addProduct(Product product){
-  this.products.add(product);
-}
-public Iterator<Product>iterator(){
-  return this.products.iterator();
-}
-product Product getProduct(String code){
-  for(Product product:this.products){
-    if(product.getCode().equals(code)){
-      return product;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Catalog implements Iterable<Product>{
+    private ArrayList<Product> products;
+    public  Catalog(){
+        this.products=new ArrayList<Product>();
     }
-  }
-  return null;
-}
-public int getNumberOfProducts(){
-  return this.products.size();
-}
+    public void addProduct(Product product){
+        this.products.add(product);
+    }
+    public Iterable<Product> itertor(){
+        return (Iterable<Product>) this.products.iterator();
+    }
+    public Product getProduct(String code){
+        for (Product product:this.products){
+            if (product.getCode().equals(code)){
+                return product;
+            }
+
+        }
+        return null;
+    }
+    public int getNumberofProducts(){
+        return this.products.size();
+    }
+
+    @Override
+    public Iterator<Product> iterator() {
+        return this.products.iterator();
+    }
+
+    public void removeProduct(Product removeCA) {
+        this.products.remove(removeCA);
+    }
 }
