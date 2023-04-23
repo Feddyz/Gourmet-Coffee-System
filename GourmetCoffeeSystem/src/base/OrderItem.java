bf25d8d9cabce5ad8c813b880786a3e7b2c3d13e
ThekinkA <110724495+ThekinkA@users.noreply.github.com>
2023-04-23 16:35:55 +0800

    Update OrderItem.java

--- GourmetCoffeeSystem/src/base/OrderItem.java
+++ GourmetCoffeeSystem/src/base/OrderItem.java
@@ -1,24 +1,28 @@
 package base;
-import java.util.*;
 
 public class OrderItem {
-	private Product product;
-	private int quantity = 0;
-	
-	public OrderItem(Product product,int quantity) {
-		this.product = product;
-		this.quantity = quantity;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public Product getProduct() {
-		return product;
-	}
-	public void setQuantity(int Quantity) {
-		quantity = newQuantity;
-	}
-	public double getValue() {
-		return quantity * coffeeAccessory.getPrice();
-	}
+    private static Product product;
+    private int quantity = 0;
+
+    public OrderItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public static Product getProduct() {
+        return product;
+    }
+    public void setQuantity(int newQuantity) {
+        quantity = newQuantity;
+    }
+    public double getValue() {
+        return quantity * Product.getPrice();
+    }
+
+    @Override
+    public String toString() {
+        return getQuantity()+"_"+getProduct().getCode()+"_"+getProduct().getPrice();
+    }
 }
